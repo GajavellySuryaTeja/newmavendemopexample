4181f2119b5e271144258de3e6bf89d979d606a3
GajavellySuryaTeja <gsuryateja6032@gmail.com>
2024-03-20 12:14:39 +0530

    first commit

---
+++ src/test/java/newmavendemoexamplepractice/Class1.java
@@ -0,0 +1,10 @@
+package newmavendemoexamplepractice;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is to interchange the element of node");
+
+	}
+
+}
